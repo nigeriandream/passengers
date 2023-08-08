a61a641b17cbcfcdbd1d65af5beb690cf6fc02a1
nigeriandream <53399369+nigeriandream@users.noreply.github.com>
2023-08-08 14:56:33 +0100

    added revert for notification displays

--- script.js
+++ script.js
@@ -21,6 +21,9 @@ decrementCount = () => {
         viewCount.innerHTML = `${curCount} ${passenger()}`; // Update the displayed count with the correct pluralization
     } else {
         viewCount.innerHTML = 'Already at minimum!';
+        setTimeout(() => {
+            viewCount.innerHTML = `${curCount} ${passenger()}`; // revert view after 1 second to show current count
+        }, 1000);
     }
     // curCount > 0 ? curCount-- : false; // Decrease the current count, but ensure it never goes below zero
 }
@@ -33,9 +36,15 @@ resetCount = () => {
             viewCount.innerHTML = 'Counter has been reset'; // Update the displayed count to indicate the reset
         } else {
             viewCount.innerHTML = 'Reset cancelled.'; // If reset is cancelled, show a message in the view
+            setTimeout(() => {
+                viewCount.innerHTML = `${curCount} ${passenger()}`; // revert view after 1 second to show current count
+            }, 1000);
         }
     } else {
         viewCount.innerHTML = 'Nothing to Reset!'
+        setTimeout(() => {
+            viewCount.innerHTML = `${curCount} ${passenger()}`; // revert view after 1 second to show current count
+        }, 1000);
     }
 }
 
@@ -74,6 +83,9 @@ saveCount = () => {
     }
     else { confirm("Are you sure you want to save an empty count?") === true ? addHistory() : viewCount.innerHTML = 'Nothing to Save!' }; // If the current count is greater than zero, add a history entry; otherwise, verify the user is willing to save an empty count
     toggleHistory(); // Toggle the visibility of the history container
+    setTimeout(() => {
+        viewCount.innerHTML = `${curCount} ${passenger()}`; // revert view after 1 second to show current count
+    }, 1000);
 }
 
 // Function to toggle the visibility of the history container based on the number of history entries
